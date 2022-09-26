f98f0d13c0e1794280d7b582d956f3f228f36012
KhamisOO <85188257+khamisoo@users.noreply.github.com>
2022-09-26 12:34:53 +0200

    update domain 1
    setup the google url with new domain

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/local",
+  callbackURL: "http://petsmatches.com/auth/google/local",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 }, function(accessToken, refreshToken, profile, cb) {
   User.findOrCreate({
